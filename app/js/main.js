bc0316d0b48827c2c29f3c460dc54f7bb9822fd2
itolog <paukanlove@gmail.com>
2017-12-07 20:46:09 +0100

    second commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,54 @@
 $(document).ready(() => {
-    let screen = 0,
-        container = $('.main'),
-        page = $('.page'),
-        inscroll = false;
+  let screen = 0,
+      container = $('.main'),
+      page = $('.page'),
+      inscroll = false,
+      content = $('.content h1'),
+      navUl = $('.nav__ul'),
+      navItems = $('.nav__li');
+///////////////////////////////////////////// Scroll Function
+let onScroll = () => {
+  $('body').on('mousewheel', (event) => {
+    page.children(0).addClass('scroll');
+     if (!inscroll) {
+         inscroll = true;
+         if (event.deltaY > 0) {
+             screen--;
+             page.eq(screen).addClass('scroll').siblings().removeClass('scroll');
+             content.eq(screen).addClass('roo').siblings().removeClass('roo');
+             navItems.eq(screen).addClass('active').siblings().removeClass('active');
+         } else {
+             screen++;
+             page.eq(screen).addClass('scroll').siblings().removeClass('scroll');
+             content.eq(screen).addClass('roo').siblings().removeClass('roo');
+             navItems.eq(screen).addClass('active').siblings().removeClass('active');
+         }
+         
+     }
 
-    $('body').on('mousewheel', (event) => {
-       page.children(0).addClass('scroll');
-        if (!inscroll) {
-            inscroll = true;
-            if (event.deltaY > 0) {
-                screen--;
-                page.eq(screen).addClass('scroll').siblings().removeClass('scroll');
-            } else {
-                screen++;
-                page.eq(screen).addClass('scroll').siblings().removeClass('scroll');
-            }
-        }
-        if (screen > page.length -1 || screen < 1) {
-            screen = 0;
-            page.children(0).removeClass('scroll');
-        }
-        let position = (-screen * 100) + '%';
-        container.css('top', position);
-        // container.css('transform', 'translateY('+position+')');
-        setTimeout(function() {
-            inscroll = false;
-        }, 1300);
-    })
+     // Обнуление счетчика
+     if (screen > page.length -1 || screen < 1) {
+         screen = 0;
+         page.children(0).removeClass('scroll');
+        navItems.eq(0).addClass('active').siblings().removeClass('active');
+     }
+// /////////////////////////////////////////////////
+     let position = (-screen * 100) + '%';
+     container.css('top', position);
+     // задержа скрола
+     setTimeout(function() {
+         inscroll = false;
+     }, 1300);
+ });
+};
+  //////////////////////////////////////////////  Scroll Function END
+  //  CLICK 
+  let onClick = () => {
+    content.on('click', () => {
+      $(this).hide();
+    });
+  }
+  // CLICK END
+  onScroll();
+  onClick();
 });
